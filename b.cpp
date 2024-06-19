ccf7ba13402b2d4b0ac5dbd0cdeaf39d41ba74b5
Akshat madhavan <112942320+GokuUu007@users.noreply.github.com>
2024-06-19 22:56:34 +0530

    Create b.cpp

---
+++ b.cpp
@@ -0,0 +1,85 @@
+/* 
+
+    ॐ हौं जूं सः ॐ भूर्भुवः स्वः 
+    ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय मामृतात् 
+    ॐ स्वः भुवः भूः ॐ सः जूं हौं ॐ
+
+   @Author  : Akshat Madhavan
+   @Country : INDIA 
+   
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+
+#ifdef AKSHAT
+#include "debug.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+
+
+template<class T>void output (vector<T> &a) {for (auto &x : a) {cout << x << ' ';}cout << '\n';}
+template<typename T> void chmin (T &a, T b) {if (a > b) swap(a, b);}
+template<typename T> void chmax (T &a, T b) {if (a < b) swap(a, b);}
+
+const int MAXN = 1e6 + 1;
+vector<int> spf(MAXN), primes;
+void sieve() {
+    iota(all(spf), 0);
+    for (int i = 2; i < MAXN; ++i) {
+        if (spf[i] == i) { 
+            primes.push_back(i);           
+            for (int j = 2 * i; j < MAXN; j += i)
+                spf[j] = min(i, spf[j]);
+        }
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    ll ans = 0;
+    for (auto &p : primes) {
+        if (p <= spf[n])
+            ans += n * 1LL * p;
+    }
+
+    cout << ans << '\n';
+
+} 
+
+
+int32_t main() {
+    
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    #ifdef AKSHAT   
+        freopen("error.txt", "w", stderr);  
+    #endif  
+
+    sieve();
+
+    int tc = 1;
+    cin >> tc;
+    for (int cases = 1; cases <= tc; ++cases) {
+        solve();
+        // cout << (solve() ? "YES\n" : "NO\n");
+        // cout << "Case # " << cases;
+    } 
+
+    #ifdef AKSHAT
+        double timeTaken = 1000.0 * clock() / CLOCKS_PER_SEC;
+        cout << "\n[Finished in " << timeTaken << "ms]";
+        cerr << "\n[Finished in " << timeTaken << "ms]";
+    #endif
+
+    return 0;
+}
+
+//JAI SHREE RAM !!!!
